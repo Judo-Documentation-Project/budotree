d42608edce56c44a674f642a5076fe4ade9f879c
Frederico Muñoz <fsmunoz@gmail.com>
2023-04-27 15:48:20 +0100

    Update cli args

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,12 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv))
       .command('dir', 'source directory with YAML files.')
-      .command('target', 'path of the resulting JSON file.')
+      .command('file', 'path of the resulting JSON file.')
       .help()
       .argv;
 
 var dir = argv.dir;
-var targetFile = argv.target;
+var targetFile = argv.file;
 console.log("JDP database parsing started.");
 console.log(`Database dir: ${dir}, target file: ${targetFile}`);
 //var dir  = '/home/frmuno/src/lisp/judotree/database/persons/JPN';
