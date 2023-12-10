1d77679d7ad657a4db03ca86a45140e3c7685130
Frederico Muñoz <fsmunoz@gmail.com>
2023-12-10 13:06:18 +0000

    Improve header of CSV file of broken links

--- bin/link_check.sh
+++ bin/link_check.sh
@@ -3,7 +3,7 @@
 IN=aux/Links.md
 OUT=aux/broken_links.csv
 
-echo "URL,Code,Status,Document" > $OUT
+echo "url,status,state,parent,failureDetails" > $OUT
 npx linkinator  --markdown --timeout 5000 $IN  -f CSV | grep BROKEN >> $OUT
 
 echo There are $(wc -l $OUT | cut -d' ' -f1) broken links.
