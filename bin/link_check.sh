966edeb92f31cd8e72c9b30ea0722194e8f2f002
Frederico Muñoz <fsmunoz@gmail.com>
2023-12-10 13:01:08 +0000

    Add header to CSV file of broken links

--- bin/link_check.sh
+++ bin/link_check.sh
@@ -3,6 +3,6 @@
 IN=aux/Links.md
 OUT=aux/broken_links.csv
 
-npx linkinator  --markdown --timeout 5000 $IN  -f CSV | grep BROKEN > $OUT
-
+echo "URL,Code,Status,Document" > $OUT
+npx linkinator  --markdown --timeout 5000 $IN  -f CSV | grep BROKEN >> $OUT
 echo There are $(wc -l $OUT | cut -d' ' -f1) broken links.
