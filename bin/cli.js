31095cf96b19bd152ab8a9bdf83fabd765e488af
Frederico Muñoz <fsmunoz@gmail.com>
2023-12-18 12:32:14 +0000

    Add local-only option to parsing (for CI)

--- bin/cli.js
+++ bin/cli.js
@@ -6,20 +6,30 @@ const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv))
       .command('dir', 'source directory with YAML files.')
       .command('file', 'path of the resulting JSON file.')
+      .command('local', 'if present, avoids steps that make HTTP requests (useful for CI)')
+      .boolean('local')
       .help()
       .argv;
 
 
-var dir = argv.dir;
-var targetFile = argv.file;
+let dir = argv.dir
+let targetFile = argv.file
+let localOnly = argv.local
 let stylesFile = path.join(path.dirname(targetFile), "styles.json");
 
 console.log("JDP database parsing started.");
-console.log(`Database dir: ${dir}, target file: ${targetFile}, styles file: ${stylesFile}`);
+console.log(`Database dir: ${dir}, target file: ${targetFile}, styles file: ${stylesFile}, local run: ${localOnly}`);
 
 styles = pY.importStylesYAML("database/styles");
 let database = pY.importYAML(dir);
 
-pY.updateSources(database)
-    .then(v => pY.writeJSON(pY.createCYJS(database, styles), targetFile))
+if (localOnly) {
+    console.log("Local run, avoinding external requests")
+    pY.writeJSON(pY.createCYJS(database, styles), targetFile)
+} else {
+    console.log("Regular run, updating sources")
+    pY.updateSources(database)
+        .then(v => pY.writeJSON(pY.createCYJS(database, styles), targetFile))
+}
+
 pY.writeJSON(styles, stylesFile)
