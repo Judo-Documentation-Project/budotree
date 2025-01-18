daf521cb220ab9a4b2353844bdcd639f577d79e1
Frederico Munoz <fsmunoz@gmail.com>
2025-01-18 21:16:55 +0000

    Add AGPL

--- site/src/js/app.js
+++ site/src/js/app.js
@@ -1,3 +1,22 @@
+/* Budotree: Martial Arts Lineage Tree
+
+   Copyright (C) 2022-2025, Frederico Muñoz
+   
+   This program is free software: you can redistribute it and/or
+   modify it under the terms of the GNU Affero General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public
+   License along with this program.  If not, see
+   <https://www.gnu.org/licenses/>.
+*/
+
 import data from "./tree.json";
 import dbversion from "./dbversion.json";
 import ptRes from "../i18n/pt.json";
